fac066b10a4a43eb549e2d37fb80d865f829aed4
Zottola <pablozottola66@gmail.com>
2024-01-23 19:01:28 -0300

    fix style

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -17,27 +17,35 @@ const Movie = ({ id }) => {
           <p>Cargando...</p>
         </>
       ) : (
-        <div>
+        <>
           <div className="relative">
             <img
-              className="w-96 mt-8"
-              src={movies.image?.original}
+              className="w-96 max-md:mt-8 rounded-lg "
+              src={movies.image?.medium}
               alt={movies.name}
             />
             <StarRating star={movies.rating?.average} />
           </div>
 
           <div className="text-sm text-left w-96">
-            <h1 className="text-2xl my-4 text-center">{movies.name}</h1>
-            <p className="font-bold">Lenguaje: {movies.language}</p>
-            <p className="font-bold">{`Géneros: ${movies.genres?.join(
+            <h1 className="max-md:text-2xl text-5xl my-4 text-center">
+              {movies.name}
+            </h1>
+            <p className="font-bold text-base opacity-90">
+              Lenguaje: {movies.language}
+            </p>
+            <p className="font-bold text-base opacity-90">{`Géneros: ${movies.genres?.join(
               " "
             )} `}</p>
-            <p className="font-bold">Fecha de estreno: {movies.premiered}</p>
-            <h2 className="text-2xl my-4 text-center">Sinopsis</h2>
+            <p className="font-bold text-base opacity-90">
+              Fecha de estreno: {movies.premiered}
+            </p>
+            <h2 className="max-md:text-2xl text-4xl my-4 text-center">
+              Sinopsis
+            </h2>
             <span dangerouslySetInnerHTML={{ __html: movies.summary }} />
           </div>
-        </div>
+        </>
       )}
     </>
   );
